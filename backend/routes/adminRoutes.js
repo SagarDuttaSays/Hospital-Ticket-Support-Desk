0053289bef341244d83d376bb6b7e6b08913f2f3
Sagar Dutta <duttasagar086@gmail.com>
2023-07-10 00:12:05 +0530

    Updates

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {
-  registerAdmin
-} = require('../controllers/adminController')
-
-router.post('/', registerAdmin)
-
-module.exports = router
