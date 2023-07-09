fc4701247e812f2f86796d37ecc81de9eb9a5d8c
Sagar Dutta <duttasagar086@gmail.com>
2023-07-10 00:58:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.use('/api/admin', require('./routes/adminRoutes'))
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
   })
 } else {
   app.get('/', (_, res) => {
