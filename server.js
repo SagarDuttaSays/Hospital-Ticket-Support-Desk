172efef5f007fa206b628d4e8697ae9af7852a0d
Sagar Dutta <duttasagar086@gmail.com>
2023-07-10 20:38:17 +0530

    Updates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 require('colors')
 require('dotenv').config()
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
@@ -11,6 +12,8 @@ connectDB()
 
 const app = express()
 
+// Enable CORS for all routes
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
